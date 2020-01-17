6796ec5f01687ea6dee8d0d1a42019eb8ee2e7d0
Leandro Silva <lsilva.info@gmail.com>
2020-01-17 10:25:57 -0300

    Primeira parte

--- src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -11,6 +11,7 @@ import java.util.concurrent.ExecutionException;
 public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
+        
         var producer = new KafkaProducer<String, String>(properties());
 
         var value = "12345,67890, 12345234523";
