8865d2f9c9bb0214e3cd9fd86cb5ea3decf32906
Leandro <lsilva.info@gmail.com>
2020-02-09 21:33:22 -0300

    Separação dos serviços em módulos

---
+++ service-fraude/src/main/java/br/com/alura/ecommerce/FraudeDetectorService.java
@@ -0,0 +1,39 @@
+package br.com.alura.ecommerce;
+
+import br.com.alura.ecommerce.model.Order;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+import java.util.Map;
+import java.util.Properties;
+
+public class FraudeDetectorService {
+
+    public static void main(String[] args) {
+        var fraudService = new FraudeDetectorService();
+        try(var service = new KafkaService<>(FraudeDetectorService.class.getSimpleName(),
+                "ECOMMERCE_NEW_ORDER",
+                fraudService::parse,
+                Order.class,
+                Map.of())){
+            service.run();
+        }
+    }
+
+    private void parse(ConsumerRecord<String, Order> record) {
+        System.out.println("------------------------------------------");
+        System.out.println("Processing new order, checking for fraud");
+        System.out.println(record.key());
+        System.out.println(record.value());
+        System.out.println(record.partition());
+        System.out.println(record.offset());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            // ignoring
+            e.printStackTrace();
+        }
+        System.out.println("Order processed");
+    }
+
+}
--- service-order/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ service-order/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -11,22 +11,17 @@ public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         try(var orderDispatcher = new KafkaDispatcher<Order>()){
-            try(var emailDispacher = new KafkaDispatcher<String>()) {
+            try(var emailDispatcher = new KafkaDispatcher<String>()) {
                 for (int i = 0; i < 10; i++) {
-
-                    var key = UUID.randomUUID().toString();
                     var userId = UUID.randomUUID().toString();
                     var orderId = UUID.randomUUID().toString();
-
                     var amount = new BigDecimal(Math.random() * 5000 + 1);
 
                     var order = new Order(userId, orderId, amount);
-
-                    var value = key + ",67523,1234";
                     orderDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
 
                     var email = "Thank you for your order! We are processing your order!";
-                    emailDispacher.send("ECOMMERCE_SEND_EMAIL", userId, email);
+                    emailDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, email);
                 }
             }
         }
